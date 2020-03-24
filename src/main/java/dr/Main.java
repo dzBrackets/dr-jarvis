a5a254fadde57a390dd6369c97dddb831d608fc2
daouadji506 <daouadji506@gmail.com>
2020-03-24 22:40:00 +0100

    side panel in gradel (fixing lib importing ) tags deleted quick button fix

---
+++ src/main/java/dr/Main.java
@@ -0,0 +1,24 @@
+package dr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("/dr/main_panel.fxml"));
+        Scene scene=new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/dr/MainPanel.java
@@ -0,0 +1,4 @@
+package dr;
+
+public class MainPanel {
+}
