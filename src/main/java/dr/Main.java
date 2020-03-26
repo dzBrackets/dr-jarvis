26933795d521e5180fff2738d347a8cbe59e85e6
daouadji506 <daouadji506@gmail.com>
2020-03-26 22:18:29 +0100

    fixing font of text and promet text of text_field adding save and print button

--- src/main/java/dr/Main.java
+++ src/main/java/dr/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +19,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root= FXMLLoader.load(getClass().getResource("/dr/quick_panel.fxml"));
         Scene scene=new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
