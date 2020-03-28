8f363d014697d155448574ceca46a1c9e3b8464d
daouadji506 <daouadji506@gmail.com>
2020-03-28 05:32:08 +0100

    adding Auto Complete TextField in search panel

--- src/main/java/dr/PatientSearch.java
+++ src/main/java/dr/PatientSearch.java
@@ -1,4 +1,32 @@
 package dr;
 
-public class PatientSearch {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import org.controlsfx.control.textfield.TextFields;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PatientSearch implements Initializable {
+    public Label search_label;
+    @FXML
+     private JFXTextField search_TF;
+     @FXML
+     private Pane serach_panel;
+     @FXML
+     private JFXButton add_btn;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        String [] possible_list= {"aymen", "brahim", "daouadji","team","dzb"};
+        TextFields.bindAutoCompletion(search_TF,possible_list);
+    }
+
+    public void onClick(ActionEvent e) {
+
+    }
 }
