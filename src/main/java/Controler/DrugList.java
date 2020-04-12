6b23b50e8d321c4f4b6b6ce569dd2a454715d803
daouadji506 <daouadji506@gmail.com>
2020-04-12 14:58:45 +0100

    adding New_drugs.fxml styling css problem in combo_box font-color fixing combo_boxes in quick_panel like figma adding New_drugs.fxml to drug_list page(click the button) adding FXML folder in res

--- src/main/java/Controler/DrugList.java
+++ src/main/java/Controler/DrugList.java
@@ -4,16 +4,24 @@ import com.jfoenix.controls.JFXButton;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import model.drug;
 import model.prescription;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -58,6 +66,15 @@ public class DrugList  implements Initializable {
         drug_table.setEditable(true);
     }
 
-    public void add_drug_table(ActionEvent actionEvent) {
+    public void add_drug_table(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/dr/FXML/POPUP/New_drugs.fxml"));
+        Scene sc =new Scene(root);
+        sc.setFill(Color.TRANSPARENT);
+        sc.getStylesheets().add("org/kordamp/bootstrapfx/bootstrapfx.css");
+        Stage s=new Stage();
+        s.initModality(Modality.APPLICATION_MODAL);
+        s.setScene(sc);
+        s.initStyle(StageStyle.TRANSPARENT);
+        s.show();
     }
 }
