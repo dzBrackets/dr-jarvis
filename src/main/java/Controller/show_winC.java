9f8318b29356517eaf6e75799b494f601a25e7c4
daouadji506 <daouadji506@gmail.com>
2020-04-23 22:02:16 +0100

    show panel

--- src/main/java/Controller/show_winC.java
+++ src/main/java/Controller/show_winC.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -8,6 +9,10 @@ import java.util.ResourceBundle;
 public class show_winC implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void exit_methode(ActionEvent actionEvent) {
 
     }
 }
