7169f4b7493eec2b4ffd1de6c5d46156b8951de7
daouadji506 <daouadji506@gmail.com>
2020-05-25 23:10:09 +0100

    setting page done

--- src/main/java/Controller/settingC.java
+++ src/main/java/Controller/settingC.java
@@ -1,13 +1,35 @@
 package Controller;
 
+import javafx.event.Event;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class settingC implements Initializable {
+    public Label tab_type;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+
+    public void personal_selected(Event event) {
+        tab_type.setText("Personal ");
+    }
+
+    public void preferences_selected(Event event) {
+        tab_type.setText("Preferences ");
+    }
+
+    public void customize_selected(Event event) {
+        tab_type.setText("customize ");
+    }
+
+    public void infoTab_selected(Event event) {
+        tab_type.setText("info");
     }
 }
