587462b546a466cdec3fa73de55939067cc5f934
daouadji506 <daouadji506@gmail.com>
2020-06-01 12:24:59 +0100

    edit function in setting page

--- src/main/java/Controller/settingC.java
+++ src/main/java/Controller/settingC.java
@@ -1,15 +1,47 @@
 package Controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class settingC implements Initializable {
     public Label tab_type;
+    public JFXButton import_btn;
+    public JFXButton export_btn;
+    public JFXButton cancel_btn;
+    public JFXButton save_btn;
+    public JFXTextField name_field;
+    public JFXTextField email_field;
+    public JFXTextField adr_field;
+    public JFXTextField phone_field;
+    public GridPane fields_gridpane;
+    public AnchorPane setting_pane;
+    public Tab personal_tab;
+    public JFXButton edit_btn;
+    public GridPane labels_gridpane;
+    public Label namel_label;
+    public Label email_label;
+    public Label phone_label;
+    public Label adr_label;
+    public JFXButton load_file_btn;
+    public JFXButton load_api_btn;
+    public JFXButton buy_btn;
+    public JFXButton clean_btn;
+    public Tab Preferences_tab;
+    public JFXComboBox timezone;
+    public JFXComboBox timeformat;
+    public Tab customize_tab;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -29,5 +61,37 @@ public class settingC implements Initializable {
 
     public void infoTab_selected(Event event) {
         tab_type.setText("info");
+    }
+
+    public void edit_doc(ActionEvent actionEvent) {
+        edit_btn.setVisible(false);
+        save_btn.setVisible(true);
+        cancel_btn.setVisible(true);
+        labels_gridpane.setVisible(false);
+        fields_gridpane.setVisible(true);
+        name_field.setText(namel_label.getText());
+        email_field.setText(email_label.getText());
+        adr_field.setText(adr_label.getText());
+        phone_field.setText(phone_label.getText());
+    }
+
+    public void cancel_edit(ActionEvent actionEvent) {
+        fields_gridpane.setVisible(false);
+        labels_gridpane.setVisible(true);
+        save_btn.setVisible(false);
+        cancel_btn.setVisible(false);
+        edit_btn.setVisible(true);
+    }
+
+    public void save_edit(ActionEvent actionEvent) {
+        namel_label.setText(name_field.getText());
+        email_label.setText(email_field.getText());
+        adr_label.setText(adr_field.getText());
+        phone_label.setText(phone_field.getText());
+        save_btn.setVisible(false);
+        cancel_btn.setVisible(false);
+        edit_btn.setVisible(true);
+        fields_gridpane.setVisible(false);
+        labels_gridpane.setVisible(true);
     }
 }
