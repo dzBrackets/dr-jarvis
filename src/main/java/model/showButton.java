e3d78bdf5813ce414afd6e4a49cfc42be5e0b724
daouadji506 <daouadji506@gmail.com>
2020-06-08 15:28:05 +0100

    welcome page 2

--- src/main/java/model/showButton.java
+++ src/main/java/model/showButton.java
@@ -5,6 +5,7 @@ import com.jfoenix.controls.JFXButton;
 public class showButton extends JFXButton {
     public showButton(String text){
         this.setText(text);
+        this.setFocusTraversable(false);
         this.getStyleClass().add("show_btn");
     }
 }
